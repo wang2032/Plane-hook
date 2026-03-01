b3375217454eaa8ece06552fad1199cd8fdd96c5
Ubuntu <ubuntu@localhost.localdomain>
2026-03-01 11:40:24 +0800

    feat:添加业务部hook

--- src/routes/planeRouter.js
+++ src/routes/planeRouter.js
@@ -5,4 +5,7 @@ const messageController = require('../controllers/messageController');
 // 消息钩子路由
 router.post('/message-hook', messageController.receiveMessage);
 
+//业务部消息回调
+router.post('/message-bushook', messageController.receiveMessage);
+
 module.exports = router;
